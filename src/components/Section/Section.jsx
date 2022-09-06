e0a4dc634502ba5085f75e630a15b759aa4e561a
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-06 09:26:51 +0200

    Section - code refactor

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,19 +1,17 @@
 import styles from './Section.module.css';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
-class Section extends Component {
-  render() {
-    const { heading } = styles;
-    const { title, children } = this.props;
-    return (
-      <section>
-        <h2 className={heading}>{title}</h2>
-        {children}
-      </section>
-    );
-  }
-}
+const Section = ({ title, children }) => {
+  const { heading } = styles;
+  return (
+    <section>
+      <h2 className={heading}>{title}</h2>
+      {children}
+    </section>
+  );
+};
+
 Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
